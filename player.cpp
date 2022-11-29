85d2afa389f336d4e6d5cb1d77277737143252f8
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2022-11-29 22:36:51 +0200

    Made player fatter.

--- player.cpp
+++ player.cpp
@@ -48,58 +48,61 @@ class Player {
         }
 
         // check if collides with ball
-        if(curX > ball->curX - 0.01 && curX < ball->curX + 0.01) {
-            if(curZ > ball->curZ - 0.01 && curZ < ball->curZ + 0.01) {
-                ball->kick(dirX, dirZ, 50);
-            }
+        double distToBall = sqrt(pow(curX - ball->curX, 2) + pow(curZ - ball->curZ, 2));
+        if(distToBall <= 0.01) {
+            ball->kick(dirX, dirZ, 50);
         }
     }
 
     void draw() {
         glPushMatrix();
-        glTranslated(curX, curY, curZ);
-        glRotated(angle * 180/M_PI, 0, 1, 0);
+            glTranslated(curX, curY, curZ);
+            glRotated(angle * 180/M_PI, 0, 1, 0);
 
-        glPushMatrix();
-        glTranslated(0, 0.09, 0);
-        glColor3f(0.88, 0.67, 0.41);
-        glutSolidSphere(0.01, 50, 50);
-        glPopMatrix();
+            // Face
+            glPushMatrix();
+                glTranslated(0, 0.095, 0);
+                glColor3f(0.88, 0.67, 0.41);
+                glutSolidSphere(0.01, 50, 50);
+            glPopMatrix();
 
-        glPushMatrix();
-        glTranslated(0, 0.07, 0);
-        glScaled(1.5, 2, 1.5);
-        glColor3f(1, 0, 0);
-        glutSolidCube(0.01f);
-        glPopMatrix();
+            // Body
+            glPushMatrix();
+                glTranslated(0, 0.07, 0);
+                glScaled(2.5, 3.5, 1.5);
+                glColor3f(1, 0, 0);
+                glutSolidCube(0.01f);
+            glPopMatrix();
 
-        glPushMatrix();
-        glTranslated(0.01, 0.07, 0);
-        glScaled(0.5, 1.5, 0.5);
-        glColor3f(0.88, 0.67, 0.41);
-        glutSolidCube(0.01f);
-        glPopMatrix();
+            // arm one
+            glPushMatrix();
+                glTranslated(0.015, 0.075, 0);
+                glScaled(0.5, 1.5, 0.5);
+                glColor3f(0.88, 0.67, 0.41);
+                glutSolidCube(0.01f);
+            glPopMatrix();
 
-        glPushMatrix();
-        glTranslated(-0.01, 0.07, 0);
-        glScaled(0.5, 1.5, 0.5);
-        glColor3f(0.88, 0.67, 0.41);
-        glutSolidCube(0.01f);
-        glPopMatrix();
+            // arm two
+            glPushMatrix();
+                glTranslated(-0.015, 0.075, 0);
+                glScaled(0.5, 1.5, 0.5);
+                glColor3f(0.88, 0.67, 0.41);
+                glutSolidCube(0.01f);
+            glPopMatrix();
 
-        // eyes
-        glPushMatrix();
-        glTranslated(-0.005, 0.09, 0.009);
-        glColor3f(0, 0, 0);
-        glutSolidSphere(0.001, 50, 50);
-        glPopMatrix();
+            // eyes
+            glPushMatrix();
+                glTranslated(-0.005, 0.099, 0.009);
+                glColor3f(0, 0, 0);
+                glutSolidSphere(0.001, 50, 50);
+            glPopMatrix();
 
-        // eyes 2
-        glPushMatrix();
-        glTranslated(0.005, 0.09, 0.009);
-        glColor3f(0, 0, 0);
-        glutSolidSphere(0.001, 50, 50);
-        glPopMatrix();
+            // eyes 2
+            glPushMatrix();
+                glTranslated(0.005, 0.099, 0.009);
+                glColor3f(0, 0, 0);
+                glutSolidSphere(0.001, 50, 50);
+            glPopMatrix();
 
         glPopMatrix();
     }
